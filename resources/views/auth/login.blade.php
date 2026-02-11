9253eaa064e09e70e7da5df4df917b168ae7723a
vishakha <aurevion.72@gmail.com>
2026-02-11 18:10:16 +0530

    logo changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,26 +32,17 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-between mt-4">
-    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" 
-       href="{{ route('register') }}">
-        {{ __('Register') }}
-    </a>
+            <div class="flex items-center justify-end mt-4">
+                @if (Route::has('password.request'))
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                        {{ __('Forgot your password?') }}
+                    </a>
+                @endif
 
-    <div class="flex items-center">
-        @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 me-4" 
-               href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-        @endif
-
-        <x-button>
-            {{ __('Log in') }}
-        </x-button>
-    </div>
-</div>
-
+                <x-button class="ms-4">
+                    {{ __('Log in') }}
+                </x-button>
+            </div>
         </form>
     </x-authentication-card>
 </x-guest-layout>
